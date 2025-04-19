0daf71655dcb46abf790ae770b3579461f1c6992
JagdeepSingh13 <jagdeeps3105@gmail.com>
2025-04-19 22:13:33 +0530

    login user controller, validation, route

--- 04microservices/identity-service/src/controllers/identity-controller.js
+++ 04microservices/identity-service/src/controllers/identity-controller.js
@@ -1,5 +1,5 @@
 const logger = require("../utils/logger");
-const { validateRegistration } = require("../utils/validation");
+const { validateRegistration, validatelogin } = require("../utils/validation");
 const User = require("../models/User");
 const generateTokens = require("../utils/generateToken");
 
@@ -48,4 +48,53 @@ const registerUser = async (req, res) => {
   }
 };
 
-module.exports = { registerUser };
+// Login
+const loginUser = async (req, res) => {
+  logger.info("Login endpoint hit...");
+  try {
+    const { error } = validatelogin(req.body);
+    if (error) {
+      logger.warn("Validation error", error.details[0].message);
+      return res.status(400).json({
+        success: false,
+        message: error.details[0].message,
+      });
+    }
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      logger.warn("Invalid user");
+      return res.status(400).json({
+        success: false,
+        message: "Invalid credentials",
+      });
+    }
+
+    // user valid password or not
+    const isValidPassword = await user.comparePassword(password);
+    if (!isValidPassword) {
+      logger.warn("Invalid password");
+      return res.status(400).json({
+        success: false,
+        message: "Invalid password",
+      });
+    }
+
+    const { accessToken, refreshToken } = await generateTokens(user);
+
+    res.json({
+      accessToken,
+      refreshToken,
+      userId: user._id,
+    });
+  } catch (e) {
+    logger.error("Login error occured", e);
+    res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
+  }
+};
+
+module.exports = { registerUser, loginUser };
